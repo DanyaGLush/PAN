36a4be8a2b0eca83597d2892ec03bba616285ceb
Даниил Глушенков <glushenkovdv05@gmail.com>
2025-12-14 20:27:42 +0300

    task1

---
+++ SEM2/task1/Task1_sem2.cpp
@@ -0,0 +1,24 @@
+﻿
+
+#include <iostream>
+#include <cmath> 
+
+
+using namespace std;
+
+int main()
+{
+    double S, V, p, C, L;
+    cout << "Площадь крыла: ";
+    cin >> S;
+    cout << "Скорость полета: ";
+    cin >> V;
+    cout << "Плотность воздуха: ";
+    cin >> p;
+    cout << "Коэффициент подъемной силы: ";
+    cin >> C; 
+    L = 0.5 * p * S * C * pow(V,2);
+    cout << "Подъемная сила: " << L << endl;
+    
+}
+
